9c343978ba50d8e45ae8d933099f021cf29e242c
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-10-14 00:09:05 +0700

    buat model asset type beserta relasi nya

---
+++ app/Models/AssetType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Lumen\Auth\Authorizable;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    protected $table = 'asset_type';
+    public $timestamps = false;
+
+    public function asset(){
+        return $this->hasMany(Asset::class, 'type_id');
+    }
+}
