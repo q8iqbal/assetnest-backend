dce2388ce2f7865402934c91da3623731053df6b
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-10-14 18:51:52 +0700

    Auto stash before checking out "HEAD"
    rubah relasi di table company

--- database/migrations/2020_10_11_183629_update_company_table.php
+++ database/migrations/2020_10_11_183629_update_company_table.php
@@ -6,29 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 class UpdateCompanyTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('company', function (Blueprint $table) {
-            $table->foreign('owner_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
-        });
-    }
+    // /**
+    //  * Run the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function up()
+    // {
+    //     Schema::table('company', function (Blueprint $table) {
+    //         $table->foreign('owner_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('company', function(Blueprint $table){
-            $table->dropForeign('company_owner_id_foreign');
-            $table->dropIndex('company_owner_id_foreign');
-            $table->dropColumn('owner_id');
-        });
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function down()
+    // {
+    //     Schema::table('company', function(Blueprint $table){
+    //         $table->dropForeign('company_owner_id_foreign');
+    //         $table->dropIndex('company_owner_id_foreign');
+    //         $table->dropColumn('owner_id');
+    //     });
+    // }
 }
