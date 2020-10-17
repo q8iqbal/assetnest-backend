5bb2bc63737559fde268d59c1dc25d8bff6c38ce
GoldyNajma <najma.goldy465@gmail.com>
2020-10-17 20:09:41 +0700

    Add CRUD Feature for Company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,12 +14,42 @@ class CompanyController extends Controller
      */
     public function __construct()
     {
-
+        $this->middleware('auth');
     }
 
-    public function index(){}
-    public function show($id){}
-    public function store(Request $request){}
-    public function update(Request $request, $id){}
-    public function destroy($id){}
+    public function index()
+    {
+        $companies = Company::all();
+
+        return response()->json($companies);
+    }
+    
+    public function show($id)
+    {
+        $company = Company::find($id);
+
+        return response()->json($company);
+    }
+
+    public function store(Request $request)
+    {
+        $company = Company::create($request->all());
+
+        return response()->json($company, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->update($request->all());
+
+        return response()->json($company, 200);
+    }
+
+    public function destroy($id)
+    {
+        Company::findOrFail($id)->delete();
+
+        return response('Deleted Successfully', 200);
+    }
 }
