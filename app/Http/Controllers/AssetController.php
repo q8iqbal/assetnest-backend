63cca37bfea02855c217eefbaa43e246895f1199
Nawa <dragneel77@gmail.com>
2020-10-17 20:58:42 +0700

    CRUD Asset
    nambahi routing di web.php, ngisi AssetController, mbenahi model asset.php

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -17,10 +17,89 @@ class AssetController extends Controller
     }
 
     public function index(){
-        return Asset::all();
+        $assets = Asset::all();
+    
+        $out = [
+            "massage" => "list_asset",
+            "result" => $assets,
+        ];
+
+        return response()->json($out, 200); 
     }
-    public function show($id){}
-    public function store(Request $request){}
-    public function update(Request $request, $id){}
-    public function destroy($id){}
+    public function show($id){
+        $asset = Asset::find($id);
+
+        $out = [
+            "massage" => "asset_".$id,
+            "result" => $asset,
+        ];
+
+        return response()->json($out, 200); 
+    }
+    public function store(Request $request){
+        if($request->isMethod('post')){
+            $this->validateJson($request, 'asset' , Asset::getValidateRules());
+            
+            $asset = $request->json()->get('asset');
+            $insert = Asset::create($asset);
+            
+            if($insert){
+                $out = [
+                    "message" => "success_insert_data",
+                    "result" => $asset,
+                    "code" => 200,
+                ];
+            } else {
+                $out = [
+                    "message" => "vailed_insert_data",
+                    "result" => $asset,
+                    "code" => 404,
+                ];
+            }
+            
+            return response()->json($out, $out['code']);
+        }
+    }
+    public function update(Request $request, $id){
+        if($request->isMethod('put')){
+            $this->validateJson($request, 'asset' ,Asset::getValidateRules());
+            
+            $assetNew = $request->json()->get('asset');
+            $asset = Asset::find($id);
+
+            $update = $asset->update($assetNew);
+
+            if($update){
+                $out = [
+                    "message" => "success_update_data",
+                    "result" => $asset,
+                    "code" => 200,
+                ];
+            } else {
+                $out = [
+                    "message" => "vailed_update_data",
+                    "result" => $asset,
+                    "code" => 404,
+                ];
+            }
+
+            return response()->json($out, $out['code']);
+        }
+    }
+    public function destroy($id){
+        $asset = Asset::find($id);
+
+        if(!$asset){
+            $data = [
+                "message" => "id not found",
+            ];
+        }else{
+            $asset->delete();
+            $data = [
+                "message" => "success_deleted",
+            ];
+        }
+    
+        return response()->json($data, 200);
+    }
 }
