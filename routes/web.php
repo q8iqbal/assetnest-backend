6f79f901e89d66db868a771f42c2d88753fe2161
Febilenia <febileniarismapa@gmail.com>
2020-10-18 16:34:44 +0700

    UserController+CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Models\Company;
+use App\Models\User;
+use Laravel\Lumen\Routing\Router;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +18,60 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->post('login', 'AuthController@login');
+$router->post('register', 'AuthController@register');
+$router->get('logout', 'AuthController@logout');
+$router->post('tes', function(Request $request){
+    return response($request->all());
 });
 
-// $router->get('/key', function(){
-//     return \Illuminate\Support\Str::random(32);
-// });
+
+$router->group(['prefix' => 'user'], function () use ($router){
+    $router->get('/', ['uses' => 'UserController@index']);
+    $router->get('/{id}', ['uses' => 'UserController@show']);
+    $router->post('/', ['uses' => 'UserController@store']);
+    $router->put('/{id}', ['uses' => 'UserController@update']);
+    $router->delete('/{id}', ['uses' => 'UserController@destroy']);
+});
+
+$router->group(['prefix' => 'company'], function () use ($router){
+    $router->get('/', ['uses' => 'CompanyController@index']);
+    $router->get('/{id}', ['uses' => 'CompanyController@show']);
+    $router->post('/', ['uses' => 'CompanyController@store']);
+    $router->put('/{id}', ['uses' => 'CompanyController@update']);
+    $router->delete('/{id}', ['uses' => 'CompanyController@destroy']);
+});
+
+$router->group(['prefix' => 'asset'], function () use ($router){
+    $router->get('/', 'AssetController@index');
+
+    $router->get('/{id}','AssetController@show');
+
+    $router->post('/', 'AssetController@store');
+
+    $router->put('/{id}', 'AssetController@update');
+
+    $router->delete('/{id}', 'AssetController@destroy');
+});
+
+$router->group(['prefix' => 'assethist'], function () use ($router){
+    $router->get('/', function(){
+        return 'get';
+    });
+
+    $router->get('/{id}', function($id){
+        return "get $id";
+    });
+
+    $router->post('/', function(){
+        return 'post';
+    });
+
+    $router->put('/{id}', function($id){
+        return "put $id";
+    });
+
+    $router->delete('/{id}', function($id){
+        return "delete $id";
+    });
+});
