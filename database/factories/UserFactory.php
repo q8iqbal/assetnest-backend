2cea8a60ba1e0782dd8c452a27a93304bcaa6b2a
Febilenia <febileniarismapa@gmail.com>
2020-10-19 08:22:23 +0700

    dicek lagi ya rek

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,29 +1,12 @@
 <?php
-
-namespace Database\Factories;
-
-use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
-
-class UserFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-        ];
-    }
-}
+ 
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+	return [
+		'company-id'			=> str_random(32),
+        'name'             		=> $faker->name,
+        'email'                 => $faker->safeEmail,
+        'password'              => \Illuminate\Support\Facades\Hash::make('test-password'),
+        'role'                  => \App\Models\User::BASIC_ROLE,
+        'picture'          		=> $faker->imageUrl('100')
+    ];
+});
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,19 @@
 <?php
-
-namespace Database\Seeders;
-
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
-
+ 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call('UsersTableSeeder');
-    }
+	/**
+	* Run the database seeds.
+	*
+	* @return void
+	*/
+	public function run()
+	{
+		Model::unguard();
+		// Register the user seeder
+		$this->call(UsersTableSeeder::class);
+		Model::reguard();
+	}
 }
